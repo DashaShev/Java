ce3d3dc279d3431eaf60e4c6b22b0c7dd36c074c
DashaShev <sdasha321@gmail.com>
2016-05-18 21:13:41 +0200

    Create Sloik.java

---
+++ 07/Sloik.java
@@ -0,0 +1,63 @@
+
+public class Sloik {
+	Dzem dzem;
+	boolean czyOtwarty;
+	
+	public Sloik(Dzem dzem){
+		this.dzem = dzem;
+	}
+	
+	public Sloik() {
+		
+	}
+
+	public void otworz(){
+		czyOtwarty = true;
+	}
+	
+	public void zamknij(){
+		czyOtwarty = false;
+	}
+	
+	public boolean czyOtwarty(){
+		return czyOtwarty;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Sloik sloik1 = new Sloik();
+		if(sloik1.czyOtwarty == false){
+			sloik1.otworz();
+			sloik1 = new Sloik(new Dzem("truskawkowy"));
+			sloik1.zamknij();
+			
+		} else {
+			sloik1.zamknij();
+		}
+		
+		Sloik sloik2 = new Sloik();
+		if(sloik2.czyOtwarty == false){
+			sloik2.otworz();
+			sloik2 = new Sloik(new Dzem("bananowy", 230.0));
+			sloik2.zamknij();
+			
+		}else {
+			sloik1.zamknij();
+		}
+		
+		Sloik sloik3 = new Sloik();
+		if(sloik3.czyOtwarty == false){
+			sloik3.otworz();
+			sloik3 = new Sloik(new Dzem(200.0));
+			sloik3.zamknij();
+			
+		}else {
+			sloik1.zamknij();
+		}
+
+	}
+
+}
