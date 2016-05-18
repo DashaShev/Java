40b4288374393a8166844cf6aab52b97c4b37e30
DashaShev <sdasha321@gmail.com>
2016-05-18 21:15:39 +0200

    Update Sloik.java

--- 07/Sloik.java
+++ 07/Sloik.java
@@ -1,35 +1,6 @@
-
-public class Sloik {
-	Dzem dzem;
-	boolean czyOtwarty;
-	
-	public Sloik(Dzem dzem){
-		this.dzem = dzem;
-	}
-	
-	public Sloik() {
-		
-	}
-
-	public void otworz(){
-		czyOtwarty = true;
-	}
-	
-	public void zamknij(){
-		czyOtwarty = false;
-	}
-	
-	public boolean czyOtwarty(){
-		return czyOtwarty;
-		
-	}
-	
-	
-	
-	
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		Sloik sloik1 = new Sloik();
-		if(sloik1.czyOtwarty == false){
+		if(sloik1.czyOtwarty() == false){
 			sloik1.otworz();
 			sloik1 = new Sloik(new Dzem("truskawkowy"));
 			sloik1.zamknij();
@@ -39,7 +10,7 @@ public class Sloik {
 		}
 		
 		Sloik sloik2 = new Sloik();
-		if(sloik2.czyOtwarty == false){
+		if(sloik2.czyOtwarty() == false){
 			sloik2.otworz();
 			sloik2 = new Sloik(new Dzem("bananowy", 230.0));
 			sloik2.zamknij();
@@ -49,7 +20,7 @@ public class Sloik {
 		}
 		
 		Sloik sloik3 = new Sloik();
-		if(sloik3.czyOtwarty == false){
+		if(sloik3.czyOtwarty() == false){
 			sloik3.otworz();
 			sloik3 = new Sloik(new Dzem(200.0));
 			sloik3.zamknij();
